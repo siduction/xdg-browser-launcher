6d22b9397ccc74854e75a9ba167681433022647a
towo <towo@siduction.org>
2011-10-14 19:21:29 +0200

    Initial commit 	new file:   debian/changelog 	new file:   debian/compat 	new file:   debian/control 	new file:   debian/copyright 	new file:   debian/install 	new file:   debian/links 	new file:   debian/rules 	new file:   debian/source/format 	new file:   debian/xdg-browser-launcher.menu 	new file:   icons/hicolor/128x128/apps/xdg-browser-launcher.png 	new file:   icons/hicolor/16x16/apps/xdg-browser-launcher.png 	new file:   icons/hicolor/22x22/apps/xdg-browser-launcher.png 	new file:   icons/hicolor/32x32/apps/xdg-browser-launcher.png 	new file:   icons/hicolor/48x48/apps/xdg-browser-launcher.png 	new file:   icons/hicolor/64x64/apps/xdg-browser-launcher.png 	new file:   icons/pixmaps/xdg-browser-launcher-16.xpm 	new file:   icons/pixmaps/xdg-browser-launcher.xpm 	new file:   icons/xdg-browser-launcher.svg 	new file:   xdg-browser-launcher.desktop 	new file:   xdg-browser-launcher.js 	new file:   xdg-browser-launcher.properties

---
+++ xdg-browser-launcher.js
@@ -0,0 +1,44 @@
+// Set default homepage
+// This must be done via very strange method: http://bugs.debian.org/425961
+//
+pref("browser.startup.homepage", "file:///usr/share/xdg-browser-launcher/xdg-browser-launcher.properties");
+
+// Override default layout.css.dpi value
+//
+// -1
+// Use the host system's logical resolution or 96, whichever is greater,
+// for interpreting dimensions specified in absolute units. (Default)
+//
+// 0
+// Use the host system's logical resolution for interpreting dimensions
+// specified in absolute units.
+//
+// >= 1 (eg. 96, 120)
+// Use this number for interpreting dimensions specified in absolute units.
+//
+pref("layout.css.dpi", 0);
+
+// Set sane font sizes
+//
+pref("font.minimum-size.x-western",    11);
+pref("font.size.variable.x-western",   12);
+
+// Override browser.tabs.closeButtons
+//
+// 0
+// Display a close button on the active tab only
+//
+// 1
+// Display close buttons on all tabs (Default)
+//
+// 2
+// Don’t display any close buttons
+//
+// 3
+// Display a single close button at the end of the tab strip (Firefox 1.x behavior)
+//
+pref("browser.tabs.closeButtons", 3);
+pref("browser.tabs.tabMinWidth",  5);
+
+// Something other than purple for visited links
+pref("browser.visited_color",     "#336666");
